7dccc7939c80be0f302b1fc95194613c38c727c8
tatsuhama <kenichi.tatsuhama@gmail.com>
2015-12-09 22:55:35 +0900

    通貨単位選択画面のスケルトン作成と呼び出し

---
+++ app/src/main/java/jp/tokushima/tatsuhama/currencyconverter/CodeChooserActivity.java
@@ -0,0 +1,17 @@
+package jp.tokushima.tatsuhama.currencyconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 通貨単位を選択する画面
+ * Created by tatsuhama on 15/12/09.
+ */
+public class CodeChooserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_chooser);
+    }
+}
--- app/src/main/java/jp/tokushima/tatsuhama/currencyconverter/MainActivity.java
+++ app/src/main/java/jp/tokushima/tatsuhama/currencyconverter/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.tokushima.tatsuhama.currencyconverter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
@@ -31,12 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.from_unit)
     void onClickFromUnitButton() {
-        Toast.makeText(this, "onClickFromUnitButton", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, CodeChooserActivity.class);
+        startActivity(intent);
     }
 
     @OnClick(R.id.to_unit)
     void onClickToUnitButton() {
-        Toast.makeText(this, "onClickToUnitButton", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, CodeChooserActivity.class);
+        startActivity(intent);
     }
 
     @OnClick(R.id.button_calc)
