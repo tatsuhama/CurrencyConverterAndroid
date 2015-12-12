a996b817e2d1832d849f5d6c110d58279c16044a
tatsuhama <kenichi.tatsuhama@gmail.com>
2015-12-12 13:26:32 +0900

    不要なインポート文を削除

--- app/src/main/java/jp/tokushima/tatsuhama/currencyconverter/MainActivity.java
+++ app/src/main/java/jp/tokushima/tatsuhama/currencyconverter/MainActivity.java
@@ -3,7 +3,6 @@ package jp.tokushima.tatsuhama.currencyconverter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
